9228a8492297059b56b9392361b255ba8d13c0a7
Ulyana Lazutina <ulyalazutina@yandex.ru>
2023-11-17 08:33:21 +0300

    Переход с авторизации на регистрацию и обратно

--- render.js
+++ render.js
@@ -94,45 +94,86 @@ export const renderFormModule = ({ isLoading, commentElementError, text }) => {
         isActive();
     }
 };
+function actionWithAuth() {
+    const commentsElement = document.querySelector(".comments");
+    commentsElement.style.display = "none";
+    const deleteButtonElement = document.querySelector(".add-form-row");
+    deleteButtonElement.style.display = "none";
+    const loginHtml = `
+<div class="wrapper">
+<h1>Авторизация</h1>
+<div class="auth__container">
+<input type="text" name="" id="login-input" placeholder="Логин">
+<input type="password" name="" id="password-input" placeholder="Пароль">
+<button type="button" id = "login-btn">Войти</button>
+<button type="button" id = "registration-btn">Регистрация</button>
+</div>
+</div>
+`;
+    document.querySelector(".add-form").innerHTML = loginHtml;
 
-export const renderLinkAuthorization = () => {
-    const form = document.querySelector(".add-form");
+    const loginBtn = document.getElementById("login-btn");
+    const registrationBtn = document.getElementById("registration-btn");
 
-    const linkHtml = `
-  <p>Чтобы добавить комментарий, <button id='auth_btn'>авторизуйтесь</button></p>
-  `;
-    form.innerHTML = linkHtml;
+    loginBtn.addEventListener("click", () => {
+        const loginInputElement = document.getElementById("login-input");
+        const passwordInputElement = document.getElementById("password-input");
+        login({
+            login: loginInputElement.value,
+            password: passwordInputElement.value,
+        })
+            .then((responseData) => {
+                setToken(responseData.user.token);
+                userName = responseData.user.name;
+            })
+            .then(() => {
+                renderFormModule({
+                    isLoading,
+                    commentElementError,
+                    text: addComments,
+                });
+                commentsElement.style.display = "flex";
+                deleteButtonElement.style.display = "flex";
+            })
+            .catch((error) => {
+                if (error.message === "Неверный логин или пароль") {
+                    alert("Неверный логин или пароль");
+                    console.log("Неверный логин или пароль");
+                    passwordInputElement.value = "";
+                }
+            });
+    });
 
-    const authBtnElement = document.getElementById("auth_btn");
+    registrationBtn.addEventListener("click", () => {
+        const wrappAuth = document.querySelector(".wrapper");
+        wrappAuth.style.display = "none";
+        const formElement = document.querySelector(".add-form");
+        formElement.innerHTML = `
+        <h1>Регистрация</h1>
+        <div class="reg_container">
+        <input type="text" name="" id="nameReg-input" placeholder="Имя">
+        <input type="text" name="" id="loginReg-input" placeholder="Логин">
+        <input type="password" name="" id="passwordReg-input" placeholder="Пароль">
+        <button type="button" id = "reg-btn">Зарегистрироваться</button>
+        <button type="button" id="authNewBtn">Войти</button>
+        </div>
+        `;
+        const regBtn = document.getElementById("reg-btn");
+        const newAuthBtn = document.getElementById("authNewBtn");
 
-    authBtnElement.addEventListener("click", () => {
-        const commentsElement = document.querySelector(".comments");
-        commentsElement.style.display = "none";
-        const deleteButtonElement = document.querySelector(".add-form-row");
-        deleteButtonElement.style.display = "none";
-        const loginHtml = `
-    <div class="wrapper">
-    <h1>Авторизация</h1>
-    <div class="auth__container">
-    <input type="text" name="" id="login-input" placeholder="Логин">
-    <input type="password" name="" id="password-input" placeholder="Пароль">
-    <button type="button" id = "login-btn">Войти</button>
-    <button type="button" id = "registration-btn">Регистрация</button>
-    </div>
-    </div>
-    `;
-        form.innerHTML = loginHtml;
+        newAuthBtn.addEventListener("click", () => {
+            actionWithAuth();
+        });
 
-        const loginBtn = document.getElementById("login-btn");
-        const registrationBtn = document.getElementById("registration-btn");
-
-        loginBtn.addEventListener("click", () => {
-            const loginInputElement = document.getElementById("login-input");
-            const passwordInputElement =
-                document.getElementById("password-input");
-            login({
-                login: loginInputElement.value,
-                password: passwordInputElement.value,
+        regBtn.addEventListener("click", () => {
+            const nameRegElement = document.getElementById("nameReg-input");
+            const loginRegElement = document.getElementById("loginReg-input");
+            const passRegElement = document.getElementById("passwordReg-input");
+            console.log(loginRegElement.value);
+            registration({
+                name: nameRegElement.value,
+                login: loginRegElement.value,
+                password: passRegElement.value,
             })
                 .then((responseData) => {
                     setToken(responseData.user.token);
@@ -148,65 +189,24 @@ export const renderLinkAuthorization = () => {
                     deleteButtonElement.style.display = "flex";
                 })
                 .catch((error) => {
-                    if (error.message === "Неверный логин или пароль") {
-                        alert("Неверный логин или пароль");
-                        console.log("Неверный логин или пароль");
-                        passwordInputElement.value = "";
+                    if (
+                        error.message ===
+                        "Пользователь с таким логином уже существует"
+                    ) {
+                        alert("Пользователь с таким логином уже существует");
                     }
                 });
         });
+    });
+}
+export const renderLinkAuthorization = () => {
+    const linkHtml = `
+  <p>Чтобы добавить комментарий, <button id='auth_btn'>авторизуйтесь</button></p>
+  `;
+    document.querySelector(".add-form").innerHTML = linkHtml;
 
-        registrationBtn.addEventListener("click", () => {
-            const wrappAuth = document.querySelector(".wrapper");
-            wrappAuth.style.display = "none";
-            const formElement = document.querySelector(".add-form");
-            formElement.innerHTML = `
-            <h2>Регистрация</h2>
-            <div class="reg_container">
-            <input type="text" name="" id="nameReg-input" placeholder="Имя">
-            <input type="text" name="" id="loginReg-input" placeholder="Логин">
-            <input type="password" name="" id="passwordReg-input" placeholder="Пароль">
-            <button type="button" id = "reg-btn">Зарегистрироваться</button>
-            </div>
-            `;
-            const regBtn = document.getElementById("reg-btn");
-
-            regBtn.addEventListener("click", () => {
-                const nameRegElement = document.getElementById("nameReg-input");
-                const loginRegElement =
-                    document.getElementById("loginReg-input");
-                const passRegElement =
-                    document.getElementById("passwordReg-input");
-                console.log(loginRegElement.value);
-                registration({
-                    name: nameRegElement.value,
-                    login: loginRegElement.value,
-                    password: passRegElement.value,
-                })
-                    .then((responseData) => {
-                        setToken(responseData.user.token);
-                        userName = responseData.user.name;
-                    })
-                    .then(() => {
-                        renderFormModule({
-                            isLoading,
-                            commentElementError,
-                            text: addComments,
-                        });
-                        commentsElement.style.display = "flex";
-                        deleteButtonElement.style.display = "flex";
-                    })
-                    .catch((error) => {
-                        if (
-                            error.message ===
-                            "Пользователь с таким логином уже существует"
-                        ) {
-                            alert(
-                                "Пользователь с таким логином уже существует",
-                            );
-                        }
-                    });
-            });
-        });
+    const authBtnElement = document.getElementById("auth_btn");
+    authBtnElement.addEventListener("click", () => {
+        actionWithAuth();
     });
 };
